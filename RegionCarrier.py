92988e3ba022e7d8a4b538270c721cd2915a575c
balakirankumar <53617263+balakirankumar@users.noreply.github.com>
2020-06-09 05:43:47 +0530

    Update RegionCarrier.py

--- RegionCarrier.py
+++ RegionCarrier.py
@@ -5,13 +5,7 @@ from phonenumbers import timezone
 number=input("enter no. to check location:")
 ch_number=phonenumbers.parse(number,'IN')
 print(geocoder.description_for_number(ch_number, "en"))
-from phonenumbers import carrier 
 s_num=phonenumbers.parse(number,'RO')
 print(carrier.name_for_number(s_num, "en"))
-print("hahhaha")
 gb_number = phonenumbers.parse(number, "IN")
 print(timezone.time_zones_for_number(gb_number))
-
-
-print("hahhaha")
-
