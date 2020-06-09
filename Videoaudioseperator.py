62c3044c4f211de27a7391f80252a6ea9f6e37b8
balakirankumar <balakirankumar999@gmail.com>
2020-06-09 10:24:19 +0530

    Video and Audio seperator

--- Videoaudioseperator.py
+++ Videoaudioseperator.py
@@ -5,7 +5,7 @@
 
 import moviepy.editor
 video_name=input("enter Video name :\t")
-video=moviepy.editor.VideoFileClip(video_name+".mp4")
+video=moviepy.editor.VideoFileClip(video_name)
 audio=video.audio
 audio_outname=input("Enter output file name :\t")
 audio.write_audiofile(audio_outname+".mp3")
