77c4477f45e9d3cd1cfbcfef8b89c8049f1e84d0
Kiran Kumar <balakirankumar999@gmail.com>
2020-07-31 06:43:16 +0530

    Added Comment statements

--- Qrcode_generator.py
+++ Qrcode_generator.py
@@ -6,5 +6,5 @@ import pyqrcode
 import png
 a=int(input("Enter number to generate a qrcode of your number :\t"))
 qr=pyqrcode.create(a)
-qr.png("kiran.png", scale=3)
+qr.png("Name.png", scale=3 )# scale=Size of image
 print("Done")
